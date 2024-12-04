98c529a9e485f079974f40111f35fa3dc22a5d79
Robert <robert11122002@gmail.com>
2024-12-04 14:10:41 +0000

    day04 part 2

--- Source/day04/main.cc
+++ Source/day04/main.cc
@@ -61,6 +61,43 @@ struct Solution {
     return total;
   }
 
+  auto x_mas(std::size_t row, std::size_t col) const {
+    if (data[row][col] != 'A') {
+      return false;
+    }
+
+    if (row < 1 || row >= data.size() - 1) {
+      return false;
+    }
+    if (col < 1 || col >= data[row].size() - 1) {
+      return false;
+    }
+
+    auto top_left = data[row - 1][col - 1];
+    auto top_right = data[row - 1][col + 1];
+    auto bottom_left = data[row + 1][col - 1];
+    auto bottom_right = data[row + 1][col + 1];
+
+    return ((top_left == 'M' && bottom_right == 'S') ||
+            (top_left == 'S' && bottom_right == 'M')) &&
+           ((top_right == 'M' && bottom_left == 'S') ||
+            (top_right == 'S' && bottom_left == 'M'));
+  }
+
+  auto part2() const {
+    unsigned total{};
+
+    for (auto i : std::ranges::iota_view{0UZ, data.size()}) {
+      const auto& row = data[i];
+      for (auto j : std::ranges::iota_view{0UZ, row.size()}) {
+        if (x_mas(i, j)) {
+          ++total;
+        }
+      }
+    }
+    return total;
+  }
+
   static auto
   to_array(std::string_view filename) -> std::vector<std::vector<char>> {
     std::vector<std::vector<char>> data;
@@ -90,4 +127,5 @@ struct Solution {
 auto main() -> int {
   const Solution s;
   std::cout << "Part 1: " << s.part1() << '\n';
+  std::cout << "Part 2: " << s.part2() << '\n';
 }
