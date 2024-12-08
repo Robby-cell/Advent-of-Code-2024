071838d868fe430578715f7445bff43b9df9dff8
Robert Williamson <robert11122002@gmail.com>
2024-12-08 17:02:37 +0000

    day08 part 1

--- Source/day06/main.cc
+++ Source/day06/main.cc
@@ -7,139 +7,140 @@
 #include <utility>
 #include <vector>
 
-struct Position {
-  std::uint32_t x;
-  std::uint32_t y;
+struct Vec2 {
+    std::uint32_t x;
+    std::uint32_t y;
 
-  constexpr auto operator<=>(const Position&) const = default;
-  // constexpr auto operator<(const Position& other) const -> bool {
-  //   return x < other.x || (x == other.x && y < other.y);
-  // }
+    constexpr auto operator<=>(const Vec2&) const = default;
+    // constexpr auto operator<(const Position& other) const -> bool {
+    //   return x < other.x || (x == other.x && y < other.y);
+    // }
 };
 
-template <> struct std::hash<Position> {
-  auto operator()(const Position& p) const -> size_t {
-    char buff[sizeof(p) + 1];
-    std::copy(reinterpret_cast<const char*>(&p),
-              reinterpret_cast<const char*>(&p) + sizeof(p), buff);
-    buff[sizeof(p)] = '\0';
-    return std::hash<std::string_view>{}(buff);
-  }
+template <> struct std::hash<Vec2> {
+    auto operator()(const Vec2& p) const -> size_t {
+        char buff[sizeof(p) + 1];
+        std::copy(reinterpret_cast<const char*>(&p),
+                  reinterpret_cast<const char*>(&p) + sizeof(p), buff);
+        buff[sizeof(p)] = '\0';
+        return std::hash<std::string_view>{}(buff);
+    }
 };
 
 class Solution {
-public:
-  enum class Direction { Up, Right, Down, Left };
+  public:
+    enum class Direction { Up, Right, Down, Left };
 
-  Solution() = default;
+    Solution() = default;
 
-  auto part1() const {
-    using Dir = Direction;
+    auto part1() const {
+        using Dir = Direction;
 
-    const auto rotate = [](Direction dir) -> Dir {
-      switch (dir) {
-      case Dir::Up:
-        return Dir::Right;
-      case Dir::Right:
-        return Dir::Down;
-      case Dir::Down:
-        return Dir::Left;
-      case Dir::Left:
-        return Dir::Up;
-      }
-      std::unreachable();
-    };
+        const auto rotate = [](Direction dir) -> Dir {
+            switch (dir) {
+            case Dir::Up:
+                return Dir::Right;
+            case Dir::Right:
+                return Dir::Down;
+            case Dir::Down:
+                return Dir::Left;
+            case Dir::Left:
+                return Dir::Up;
+            }
+            std::unreachable();
+        };
 
-    auto p = find_guard_position();
-    auto dir = Dir::Up;
-    std::unordered_set<Position> seen{};
+        auto p = find_guard_position();
+        auto dir = Dir::Up;
+        std::unordered_set<Vec2> seen{};
 
-    seen.insert(p);
+        seen.insert(p);
 
-    const auto move = [&]() -> bool {
-      switch (dir) {
-      case Dir::Up:
-        if (p.x == 0) {
-          return false;
+        const auto move = [&]() -> bool {
+            switch (dir) {
+            case Dir::Up:
+                if (p.x == 0) {
+                    return false;
+                }
+                --p.x;
+                break;
+            case Dir::Right:
+                if (p.y >= grid_.at(p.x).size()) {
+                    return false;
+                }
+                ++p.y;
+                break;
+            case Dir::Down:
+                if (p.x >= grid_.size()) {
+                    return false;
+                }
+                ++p.x;
+                break;
+            case Dir::Left:
+                if (p.y == 0) {
+                    return false;
+                }
+                --p.y;
+                break;
+            }
+
+            seen.insert(p);
+            if (obstacle_in_front(dir, p)) {
+                dir = rotate(dir);
+            }
+            return true;
+        };
+
+        while (move()) {
+            asm volatile("nop");
         }
-        --p.x;
-        break;
-      case Dir::Right:
-        if (p.y >= grid_.at(p.x).size()) {
-          return false;
-        }
-        ++p.y;
-        break;
-      case Dir::Down:
-        if (p.x >= grid_.size()) {
-          return false;
-        }
-        ++p.x;
-        break;
-      case Dir::Left:
-        if (p.y == 0) {
-          return false;
-        }
-        --p.y;
-        break;
-      }
 
-      seen.insert(p);
-      if (obstacle_in_front(dir, p)) {
-        dir = rotate(dir);
-      }
-      return true;
-    };
-
-    while (move()) {
-      asm volatile("nop");
+        return seen.size();
     }
 
-    return seen.size();
-  }
-
-private:
-  auto obstacle_in_front(Direction d, Position p) const -> bool {
-    switch (d) {
-    case Direction::Up:
-      if (p.y == 0) {
-        return false;
-      }
-      return grid_[p.x][p.y - 1] == '#';
-    case Direction::Right:
-      if (p.x >= grid_.size() - 1) {
-        return false;
-      }
-      return grid_[p.x + 1][p.y] == '#';
-    case Direction::Down:
-      if (p.y >= grid_[p.x].size() - 1) {
-        return false;
-      }
-      return grid_[p.x][p.y + 1] == '#';
-    case Direction::Left:
-      if (p.x == 0) {
-        return false;
-      }
-      return grid_[p.x - 1][p.y] == '#';
-    }
-  }
-
-  auto find_guard_position() const -> Position {
-    for (auto i : std::ranges::iota_view{0UZ, grid_.size()}) {
-      for (auto j : std::ranges::iota_view{0UZ, grid_[0].size()}) {
-        if (grid_[i][j] == '^') {
-          return {static_cast<std::uint32_t>(i), static_cast<std::uint32_t>(j)};
+  private:
+    auto obstacle_in_front(Direction d, Vec2 p) const -> bool {
+        switch (d) {
+        case Direction::Up:
+            if (p.y == 0) {
+                return false;
+            }
+            return grid_[p.x][p.y - 1] == '#';
+        case Direction::Right:
+            if (p.x >= grid_.size() - 1) {
+                return false;
+            }
+            return grid_[p.x + 1][p.y] == '#';
+        case Direction::Down:
+            if (p.y >= grid_[p.x].size() - 1) {
+                return false;
+            }
+            return grid_[p.x][p.y + 1] == '#';
+        case Direction::Left:
+            if (p.x == 0) {
+                return false;
+            }
+            return grid_[p.x - 1][p.y] == '#';
         }
-      }
-    };
+    }
 
-    std::unreachable();
-  }
+    auto find_guard_position() const -> Vec2 {
+        for (auto i : std::ranges::iota_view{0UZ, grid_.size()}) {
+            for (auto j : std::ranges::iota_view{0UZ, grid_[0].size()}) {
+                if (grid_[i][j] == '^') {
+                    return {static_cast<std::uint32_t>(i),
+                            static_cast<std::uint32_t>(j)};
+                }
+            }
+        };
 
-  std::vector<std::vector<char>> grid_ = Aoc::file_to_2d_array("input.txt");
+        std::unreachable();
+    }
+
+    std::vector<std::vector<char>> grid_ = Aoc::file_to_2d_array("input.txt");
 };
 
 auto main() -> int {
-  Solution s;
-  std::cout << "Part 1: " << s.part1() << '\n';
+    Solution s;
+    std::cout << "Part 1: " << s.part1() << '\n';
 }
---
+++ Source/day08/main.cc
@@ -0,0 +1,105 @@
+#include "Core.hpp"
+#include <cassert>
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <limits>
+#include <ranges>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using std::ranges::iota_view;
+
+struct Vec2 {
+    int x;
+    int y;
+
+    friend constexpr auto operator==(const Vec2& self, const Vec2& that) {
+        return self.x == that.x && self.y == that.y;
+    }
+};
+
+template <> struct std::hash<Vec2> {
+    auto operator()(const Vec2& v) const {
+        return std::hash<int>()(v.x) ^ std::hash<int>()(v.y);
+    }
+};
+
+class Solution {
+  public:
+    Solution() = default;
+
+    auto part1() const {
+        std::unordered_map<char, std::vector<Vec2>> nodes;
+
+        for (const auto i : iota_view{0UZ, grid.size()}) {
+            const auto& row = grid.at(i);
+            for (const auto j : iota_view{0UZ, row.size()}) {
+                if (grid[i][j] != '.') {
+                    nodes[grid[i][j]].emplace_back(i, j);
+                }
+            }
+        }
+
+        const auto distance = [](Vec2 p1, Vec2 p2) {
+            const auto dx = p1.x - p2.x;
+            const auto dy = p1.y - p2.y;
+            return std::sqrt((dx * dx) + (dy * dy));
+        };
+        const auto float_eq = []<typename Float>(Float that, Float equal_to)
+            requires(std::is_floating_point_v<Float>)
+        {
+            const auto d = that - equal_to;
+            static constexpr auto Epsilon =
+                std::numeric_limits<Float>::epsilon();
+            return (d > -Epsilon) && (d < Epsilon);
+        };
+
+        std::unordered_set<Vec2> set;
+
+        for (const auto i : iota_view{0UZ, grid.size()}) {
+            const auto& row = grid.at(i);
+            for (const auto j : iota_view{0UZ, row.size()}) {
+                for (const auto& [k, v] : nodes) {
+                    for (const auto& v1 : v) {
+                        for (const auto& v2 : v) {
+                            if (v1 == v2) {
+                                continue;
+                            }
+
+                            const Vec2 dv(v2.x - v1.x, v2.y - v1.y);
+                            const Vec2 dc(static_cast<int>(i) - v1.x,
+                                          static_cast<int>(j) - v1.y);
+
+                            const Vec2 dcperp(-dc.y, dc.x);
+
+                            if ((dcperp.x * dv.x + dcperp.y * dv.y) == 0) {
+                                const Vec2 origin(i, j);
+                                const auto d1 = distance(origin, v1);
+                                const auto d2 = distance(origin, v2);
+
+                                if (float_eq(d1, d2 * 2) ||
+                                    float_eq(d2, d1 * 2)) {
+                                    set.emplace(i, j);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return set.size();
+    }
+
+  private:
+    std::vector<std::vector<char>> grid = Aoc::file_to_2d_array("input.txt");
+};
+
+auto main() -> int {
+    Solution s;
+    std::cout << "Part 1: " << s.part1() << '\n';
+}
